844b98287e17ba14bd502eaefc7180643fe31034
Azamat Namiyaly <azamatnamiyaly@gmail.com>
2023-05-26 12:05:53 +0600

    fix gulp watch config

--- gulpfile.js
+++ gulpfile.js
@@ -24,8 +24,8 @@ const watcher = () => {
   gulp.watch(path.watch.files, copy);
   gulp.watch(path.watch.html, html);
   gulp.watch(path.watch.scss, scss);
-  gulp.watch(path.watch.scss, js);
-  gulp.watch(path.watch.scss, image);
+  gulp.watch(path.watch.js, js);
+  gulp.watch(path.watch.images, image);
 }
 
 const fonts = gulp.series(otfToTtf, ttfToWoff, fontStyle)
